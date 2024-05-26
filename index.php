b9450534daafa383b9b42d06ea2ca263487c0c3b
giolaranang <32018848+giolaranang@users.noreply.github.com>
2024-05-26 15:06:57 +0800

    reroute and slideshow changes
    changed slideshow from carousel to slick

--- index.php
+++ index.php
@@ -8,62 +8,31 @@ session_start();
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+
+  <script type="text/javascript" src="//code.jquery.com/jquery-1.11.0.min.js"></script>
+  <script type="text/javascript" src="//code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+  <script type="text/javascript" src="slick/slick.min.js"></script>
+  <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
     <link rel="stylesheet" href="style.css">
     <head>
        <title>Laranang Alum</title> 
     </head>
     <body>
-        <div class="scroll-bg" id="manualCalcu" >
-      <button id="loadCal" class="calculator" onclick="loadCalculator()">Calculator</button>
-      <button id="loadCal" class="calculator" onclick="loadSample()">Sample</button>
-      <center>
-      <br>
-      <h1> 
-          Cash Advance
-      </h1>
-    <form action="save.php" method="POST">
-      <div class="form-group">
-          <select name="name" class="form-control form-control-lg">
-             <option value="" disabled selected>Click here to Select an Employee</option>
-             <option value ="Engineer Francis">Engineer Francis</option>
-             <option value ="Gio">Gio</option>
-             <option value ="Jomar">Jomar</option>
-             <option value ="John">John</option>
-             <option value ="Kennedy">Kennedy</option>
-             <option value ="Ramir">Ramir</option>
-             <option value ="Richmon">Richmon</option>
-             <option value ="Kiko">Kiko</option>
-             <option value ="Anthony">Anthony</option>
-          </select>
+      <div class="scroll-bg" id="manualCalcu" >
+      <div class="menu"> 
+        <div class="column leftside-menu">         
+            <button class="" onclick="loadCalculator()">POS System</button>
+            <button class="" onclick="loadSample()">Employee Management</button>
+        </div>
+        <div class="column">
+            <button class="login">Login</button>
+        </div>
       </div>
-      <p>
-          <input placeholder="Date" type="date" name="date" id="date" required ></input>
-      </p>
-      <p>
-          <select name="price" id="price">
-             <option value ="100">100</option>
-             <option value ="200">200</option>
-             <option value ="300">300</option>
-             <option value ="400">400</option>
-             <option value ="500">500</option>
-             <option value ="600">600</option>
-             <option value ="1000">1000</option>
-             <option value ="1500">1500</option>
-             <option value ="2000">2000</option>
-          </select>
-          <div id="manualPrice">
-             <button type="button" onclick="loadPrice()">Click to enter manual price</button>
-          </div>
-      </p>
-          <input onclick="confirmalert()" type="submit" class="btn btn-success btn-lg" value="Save" />
-      </form>
-      <br>
-      <form action= "show.php">
-          <input id="showID" class="btn btn-info btn-lg" type="submit"/>
-      </form>
-      <br>
-          <Button onclick="deletealert()" id="deleteID" class="btn btn-danger btn-lg" type="button" >Delete All</button>
-    </div>
+
+<?php
+  include 'innersample.php';
+?>
 <script>
           function confirmalert(){
               let confirmalert = confirm("Are you sure?");
@@ -88,29 +57,32 @@ session_start();
               }
           }
     window.onload=function(){
-              document.getElementById("showID").value ="Show";
+             /* document.getElementById("showID").value ="Show";
               document.getElementById("deleteID").value ="Delete";
-              document.getElementById('date').valueAsDate = new Date;
-  //  alert("<?php echo "datef: ". $_SESSION["datefrom"]; ?>");
-  //  alert("<?php echo "datet: ". $_SESSION["dateto"]; ?>");
+              document.getElementById('date').valueAsDate = new Date;*/
+
+  /*  alert("<?php echo "datef: ". $_SESSION["datefrom"]; ?>");*/
+  /* alert("<?php echo "datet: ". $_SESSION["dateto"]; ?>");*/
+    /*
     <?php
-    if ($_SESSION["operation"]=='SUMMARY'){
+  
+   if (!isset($_SESSION["operation"])){
+       $_SESSION["operation"]=="basic";
+     }
+  
+  if ($_SESSION["operation"]=='SUMMARY'){
       ?> 
        loadSum();
      <?php   
-    }else{
-        
-    if (!isset($_SESSION["operation"])){
-       $_SESSION["operation"]=="basic";
-    } else if ($_SESSION["operation"]=="loadcalcu"){
+    }else if ($_SESSION["operation"]=="loadcalcu"){
     ?>
        document.getElementById("loadCal").click();
     <?php     
     }
-    }
     ?>
+*/
 }
-    
+ 
     
 function loadPrice() {
         var xhttp = new XMLHttpRequest();
@@ -141,7 +113,7 @@ function loadSample(){
        document.getElementById("manualCalcu").innerHTML = this.responseText;
     }
   };
-  xhttp.open("GET", "innersample.php", true);
+  xhttp.open("GET", "employee_management.php", true);
   xhttp.send(); 
 }
 
--- innersample.php
+++ innersample.php
@@ -1,83 +1,63 @@
 <?php
 include "dbaseConn.php";
 ?>
-<center>
-<form action="innersamplecomp.php" method="post" enctype="multipart/form-data">
+
+
+<form class="menu" action="innersamplecomp.php" method="post" enctype="multipart/form-data">
     <select name="samplechoice" class="form-control form-control-lg">
      <option value="pocket">Pocket Type</option> 
      <option value="shower">Shower Enclosure</option>
     </select>
     <input type="file" name="uploadfile" accept="image/*"></input>
     <button type="submit" class="btn btn-outline-secondary" >Save Image</button>
-<Button type="button" onclick="operations('back')"class="btn btn-outline-secondary">Back</button>
 </form>
-</center>
-<br>
-<div class="container">
-  <div id="myCarousel" class="carousel slide" data-ride="carousel">
-      <!-- Indicators -->
-    <ol class="carousel-indicators">
-      <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
-      <?php
+
+ <?php
+
       $countquery="Select count(Picture) as counted from Samples";
       $countdata= $conn->query($countquery);
       $finalcount=$countdata->fetch_assoc();
       $totalcount=$finalcount['counted'];
-      
-      for($count=2; $count<=$totalcount; $count++){
-         ?> 
-        <li data-target="#myCarousel" data-slide-to="<?php echo $count;?>">
-        </li>
-    
-     <?php
-      }
-      ?>
- 
-    </ol>
-    <?php
-$viewSample = "select * from Samples"; 
-$resultSample = $conn->query($viewSample);
-  if($resultSample->num_rows > 0){
+
+      $viewSample = "select * from Samples"; 
+      $resultSample = $conn->query($viewSample);
+    if($resultSample->num_rows > 0){
      $firstrow=$resultSample->fetch_assoc();
      $firstrowid=$firstrow['ID'];
      ?>
-    <form method="POST" action="deleteimage.php">
-     <div class="carousel-inner">
-           <div class="item active"> 
-             <img alt="Los Angeles" style="width:500px; height:400px;" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($firstrow['Picture']); ?>"/> 
-             <div style="bottom:-20px;"class="carousel-caption">
-             <input style="color:black;" type="submit" value="Delete"/>
-             <input type="hidden" name="deletedimage" value="<?php echo $firstrowid;?>"/>
-             </div>
-     </div> 
+    <form method="POST" class="" action="deleteimage.php">
+     <div class="slideshowmain slideshow">
 <?php
-    while($row = $resultSample->fetch_assoc()){
-    $otherowid=$row['ID'];
-    ?> 
+      while($row = $resultSample->fetch_assoc()){
+      $otherowid=$row['ID'];
+      ?> 
             <div class="item"> 
-            <img alt="Los Angeles" style="width:500px; height:400px;" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Picture']); ?>"/>
-            <div style="bottom:-20px;" class="carousel-caption">
-                <input style="color:black;" type="submit" value="Delete"/>
+            <img alt="Los Angeles" src="data:image/jpg;charset=utf8;base64,<?php echo base64_encode($row['Picture']); ?>"/>
+            <div class="carousel-caption">
+                
             <input type="hidden" name="deletedimages" value="<?php echo $otherowid;?>"/>
              </div>
             </div>
     </form>
-<?php } 
-}else{ ?> 
-    <p class="status error">Image(s) not found...</p> 
-  <?php } ?>
+      <?php 
+       } 
+       ?>
+    </div> 
+    <?php
+  }else{ 
+    echo("<p class='status error'>Image(s) not found...</p>");
+  } 
+    ?>
     </div>
   </div>
-
-  <!-- Left and right controls -->
-  <a class="left carousel-control" href="#myCarousel" data-slide="prev">
-    <span class="glyphicon glyphicon-chevron-left"></span>
-    <span class="sr-only">Previous</span>
-  </a><a class="right carousel-control" href="#myCarousel" data-slide="next">
-    <span class="glyphicon glyphicon-chevron-right"></span>
-    <span class="sr-only">Next</span>
-  </a>
-</div>
+?>
 
 
+<script>
+$('.slideshowmain').slick({
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+});
+</script>
  
